dd349341504d1cb5e5adc668cf94afd4d0884ff3
asnat <asnatkemuma@gmail.com>
2023-07-12 11:47:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-// Write your code here
+let num1=2
+let num2=31
+let multiply =num1*num2
+
+let random=42
+ Number.isInteger(42)
+
+ let mod=4
+ let num3=24
+ let num4=5
+ 
+
+ let max=20
+
+
+
+
+
+
+
+
+
+
+
+
+
